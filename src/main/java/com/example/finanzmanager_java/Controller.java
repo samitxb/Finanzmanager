c20c39df412d2992ec7c93426fcf04e441373b08
MaxLxr <maxweichselgartner@gmail.com>
2022-06-04 17:28:24 +0200

    first tests with scenebuilder v3

--- src/main/java/com/example/finanzmanager_java/Controller.java
+++ src/main/java/com/example/finanzmanager_java/Controller.java
@@ -1,15 +1,18 @@
 package com.example.finanzmanager_java;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Controller {
     @FXML
     private TextField loginname;
-
+    @FXML
+    private MenuItem closeApp;
     @FXML
     private Label errortext;
 
@@ -20,4 +23,13 @@ public class Controller {
     void btnGOClicked(ActionEvent event) {
         errortext.setText("Please type in your Username and Password");
     }
+
+
+    @FXML
+    void closeApp(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+
 }

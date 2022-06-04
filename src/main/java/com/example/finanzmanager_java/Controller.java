f54cfc1b1d7a4388fc18b9a5f5ba1e2012a036a4
MaxLxr <maxweichselgartner@gmail.com>
2022-06-04 17:08:45 +0200

    first tests with scenebuilder v2

--- src/main/java/com/example/finanzmanager_java/Controller.java
+++ src/main/java/com/example/finanzmanager_java/Controller.java
@@ -6,14 +6,18 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class HelloController {
+public class Controller {
     @FXML
     private TextField loginname;
 
     @FXML
+    private Label errortext;
+
+    @FXML
     private TextField password;
 
+    @FXML
     void btnGOClicked(ActionEvent event) {
-
+        errortext.setText("Please type in your Username and Password");
     }
 }

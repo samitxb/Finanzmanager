077dfffdfc42bc0bace02021ed5899f9f0f9e294
MaxLxr <maxweichselgartner@gmail.com>
2022-06-04 13:56:23 +0200

    first tests with scenebuilder

--- src/main/java/com/example/finanzmanager_java/HelloController.java
+++ src/main/java/com/example/finanzmanager_java/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.finanzmanager_java;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField loginname;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField password;
+
+    void btnGOClicked(ActionEvent event) {
+
     }
 }

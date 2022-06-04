12601e815a1342cc24266ae93909c4f31560b1f7
MaxLxr <maxweichselgartner@gmail.com>
2022-06-04 13:30:17 +0200

    creation of projekt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.finanzmanager_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.finanzmanager_java to javafx.fxml;
+    exports com.example.finanzmanager_java;
+}

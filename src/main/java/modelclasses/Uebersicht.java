99c23447b11a69d0f70f821ebd1d98b84f1ea775
Michael Irlmeier <irli.michael@web.de>
2022-06-26 13:36:42 +0200

    Added function to sum "Ausgaben" and "Einnahmen" and give value to "Übersicht"

--- src/main/java/modelclasses/Uebersicht.java
+++ src/main/java/modelclasses/Uebersicht.java
@@ -1,13 +1,41 @@
 package modelclasses;
 
+import database.JavaPostgres;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Uebersicht {
     protected static float gesamtAusgaben;
     protected static float gesamtEinnahmen;
 
- public static float ausgabenZusammenRechnen(){
+ public static float ausgabenZusammenRechnen()  {
+
+     int id = UserLogin.id;
 
 
+     try {
+         JavaPostgres javaPostgres = new JavaPostgres();
 
+         Connection con = javaPostgres.getConnection();
+         PreparedStatement pst = con.prepareStatement("SELECT Sum(ausgaben_betrag) as ausgabengesamt FROM ausgaben where user_ausgabenid=?");
+         pst.setInt(1, id);
+         ResultSet rs = pst.executeQuery();
+         if(rs.next())
+         {
+             gesamtAusgaben = rs.getFloat("ausgabengesamt");
+         }
+
+
+     }
+
+     catch (SQLException e) {
+         throw new RuntimeException(e);
+     }
+
+        System.out.println(gesamtAusgaben);
      return gesamtAusgaben;
  }
 
@@ -15,6 +43,30 @@ public class Uebersicht {
 
  public static float einnahmenZusammenRechnen(){
 
+
+     int id = UserLogin.id;
+
+
+     try {
+         JavaPostgres javaPostgres = new JavaPostgres();
+
+         Connection con = javaPostgres.getConnection();
+         PreparedStatement pst = con.prepareStatement("SELECT Sum(einnahmen_betrag) as einnahmengesamt FROM einnahmen where user_einnahmenid=?");
+         pst.setInt(1, id);
+         ResultSet rs = pst.executeQuery();
+         if(rs.next())
+         {
+             gesamtEinnahmen = rs.getFloat("einnahmengesamt");
+         }
+
+
+     }
+
+     catch (SQLException e) {
+         throw new RuntimeException(e);
+     }
+
+     System.out.println(gesamtEinnahmen);
 
      return gesamtEinnahmen;
  }
